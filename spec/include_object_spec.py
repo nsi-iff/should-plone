6e59ed105b9392418c0c5804ef0a43c50d849fe5
Rogerio Atem de Carvalho <ratembr@gmail.com>
2011-06-04 17:41:53 -0300

    include_object matcher draft, cleaned commentaries, not using Ludibrio

--- spec/include_object_spec.py
+++ spec/include_object_spec.py
@@ -1,5 +1,4 @@
 import unittest
-from ludibrio import Stub
 from should_dsl import should, should_not
 from include_object import include_object
 
@@ -11,8 +10,6 @@ class IncludeObjectSpec(unittest.TestCase):
 
     def it_checks_if_an_object_is_included_in_a_Plone_Portal(self):
         #self.failIf('object_id' in self.portal.objectIds())
-        #with Stub() as portal:
-        #    portal.objectIds() >> ['object1', 'object2']
         portal = PlonePortal()
         #should work
         portal |should| include_object('object1')
